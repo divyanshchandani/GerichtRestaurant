1a5e1df6830d270b1f390e5441f487ccc7aa8612
divyanshchandani <divyanshchandani40672@gmail.com>
2024-08-24 00:41:11 +0530

    Completed the Navbar & SubHeading Components and the Header Container for the Website

--- src/container/AboutUs/AboutUs.js
+++ src/container/AboutUs/AboutUs.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import {images} from '../../constants'
 import './AboutUs.css';
 
-const AboutUs = () => (
+const AboutUs = () => {
+  return (
   <div>
     AboutUs
   </div>
-);
+  )
+};
 
 export default AboutUs;
--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -1,11 +1,25 @@
 import React from 'react';
-
 import './Header.css';
+import {images} from '../../constants'
+import { SubHeading } from '../../components';
 
-const Header = () => (
-  <div>
-    Header
+const Header = () => {
+  return (
+  <div className='app__header app__wrapper section__padding' id='home'>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chase the new Flavour"/>
+      <h1 className='app__header-h1'>The key to Fine Dining</h1>
+      <p className='p__opensans' style={{margin:'2rem 0'}}>Experience Gericht: Where indulgence meets sophistication in every bite. Our dishes are crafted to elevate your dining experience, combining the essence of tradition with modern flair.</p>
+      <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt='Welcome Image' />
+    </div>
+    
   </div>
-);
+  )
+}
 
 export default Header;
