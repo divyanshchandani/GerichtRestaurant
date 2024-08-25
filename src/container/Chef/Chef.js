ff5449db6c0215c129e0d58f0ae2a4acf4356a7c
divyanshchandani <divyanshchandani40672@gmail.com>
2024-08-26 02:08:30 +0530

    Finished the Chef & Intro containers for the Website where the respective containers have information about the Head Chef and the Introductory Video for the Restaurant

--- src/container/Chef/Chef.js
+++ src/container/Chef/Chef.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import {images} from '../../constants'
+import { SubHeading } from '../../components';
 
 import './Chef.css';
 
-const Chef = () => (
-  <div>
-    Chef
+const Chef = () => {
+  return(
+  <div className='app__bg app__wrapper section__padding'>
+
+    <div className='app__wrapper_img app__wrapper__img-reverse'>
+      <img src={images.chef} alt='chef__image'/>
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's Word" />
+      <h1 className='headtext__cormorant'>What We Believe In</h1>
+
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt='Quote'/>
+          <p className='p__opensans'>At Gericht, we embrace the art of culinary storytelling.</p>
+        </div>
+        <p className='p__opensans'>Our team of dedicated chefs combines traditional techniques with innovative flair, using only the freshest, locally sourced ingredients to create an unforgettable dining experience. From the first bite to the last, every flavor and texture is thoughtfully balanced to delight and inspire. We invite you to join us on a journey of taste.</p>
+      </div>
+
+      <div className='app__chef-sign'>
+        <p>Kevin Luo</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt='Sign' />
+      </div>
+    </div>
   </div>
-);
+  )
+}
 
 export default Chef;
--- src/container/Intro/Intro.js
+++ src/container/Intro/Intro.js
@@ -1,11 +1,45 @@
-import React from 'react';
-
+import {React , useState, useRef} from 'react';
+import { BsFillPlayFill , BsPauseFill } from 'react-icons/bs';
+import {meal} from '../../constants'
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = useRef();
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo)
+
+    if(playVideo) //True(Video is playing)
+    {
+      vidRef.current.pause();
+    }
+    else //False(Video is not Playing)
+    {
+      vidRef.current.play();
+    }
+  }
+
+  return(
+  <div className='app__video'>
+    <video
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+     />
+
+     <div className={`app__video-overlay flex__center ${playVideo ? 'playing' : 'paused'}`}>
+      <div 
+        className='app__video-overlay_circle flex__center'
+        onClick={handleVideo}
+      >
+          {playVideo ? <BsPauseFill color='#fff' fontSize={30}/> : <BsFillPlayFill color='#fff' fontSize={30} />}
+      </div>
+     </div>
   </div>
-);
+)}
 
 export default Intro;
